790c1f579ad69c15cb1cdc00b4ad5c2451483bce
syedsaqib12 <49809570+syedsaqib12@users.noreply.github.com>
2019-09-07 05:24:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,54 @@
 window.onload = function() {
+
   var Timer = document.getElementById('time');
   var Start = document.getElementById('start');
   var Restart = document.getElementById('restart');
-  var thirtyMins = document.getElementById('thirtyMins');
+
+  var FortyFiveSecs = document.getElementById('fortyFiveSecs');
+ 
+  var SixtySecs=document.getElementById('sixtySecs');
   var ThirtySeconds=document.getElementById('thirtySeconds');
+
   var time = 0;
   var running = 0;
-   var timeLeft = 30;
+  var timeLeft = 30;
   var secs, mins, tenths;
-  
-  
-  ThirtySeconds.onclick = function() {
-	  
-	  var timeAdd=setInterval(countBegins,1000);
-  }  
-   thirtyMins.onclick = function() {
 
-   	  var timeAdd=setInterval(countBegins,1000);
+
+  ThirtySeconds.onclick = function() {
+
+	  var timeAdd = setInterval(countBegins,1000);
+  }
+
+   fortyFiveSecs.onclick = function() {
+
+   	  var timeAdd = setInterval(countFortyFiveSecs,1000);
    };
+
+   
+   SixtySecs.onclick = function() {
+
+    var timeAdd = setInterval(countSixtySecsSecs,1000);
+  };
+
+  
+  FiveMins.onclick = function() {
+
+    var timeAdd = setInterval(countBegins,1000);
+  };
+
   Start.onclick = function() {
     if(Start.innerHTML === "Play" && running === 0) {
       running = 1;
       increment()
       Start.innerHTML = "Pause";
-      
-      
+
+
     }else{
       running = 0
       Start.innerHTML = "Play";
-     
-     
+
+
     }
   };
 
@@ -42,12 +61,74 @@ window.onload = function() {
     tenths = 0;
     Timer.innerHTML = mins+":"+secs+":"+tenths;
   };
+
   running = 0
-      Start.innerHTML = "Play";
+  Start.innerHTML = "Play";
+
+  function countBegins() {
+			time++;
+			tenths = time % 10;
+		   if(timeLeft == 0)
+		   {
+			   Timer.innerHTML = "00:00:00";
+		   }
+		   else
+		   {
+        Timer.innerHTML = "00:"+parseInt(timeLeft,10)+":0"+tenths;
+        timeLeft--;
+        }
+  }
+
+  function increment() {
+    if(running === 1) {
+      setTimeout(function() {
+        time++;
+        mins = Math.floor(time / 10 / 60);
+
+        secs = Math.floor(time / 10 % 60);
+		if (mins < 10) {
+		  mins = "0" + mins;
+		}
+		if (secs < 10) {
+		  secs = "0" + secs;
+		}
+        tenths = time % 10;
+        Timer.innerHTML = mins+":"+secs+":"+"0"+tenths;
+        increment();
+      },100);
+    }
+  }
+
+  
+  function countFortyFiveSecs() {
+			time++;
+			tenths = time % 10;
+		   if(timeLeft == 0)
+		   {
+			   Timer.innerHTML = "00:00:00";
+		   }
+		   else
+		   {
+        Timer.innerHTML = "00:"+parseInt(timeLeft,15)+":0"+tenths;
+        timeLeft--;
+        }
+  }
    
    
    
-   
+   function countSixtySecsSecs() {
+			time++;
+			tenths = time % 10;
+		   if(timeLeft == 0)
+		   {
+			   Timer.innerHTML = "00:00:00";
+		   }
+		   else
+		   {
+        Timer.innerHTML = "00:"+parseInt(timeLeft,20)+":0"+tenths;
+        timeLeft--;
+        }
+  }
    /*thirtyMins.onclick = function() {
 		if(thirtyMins.innerHTML === "Play" && running === 0) {
 		  running = 1;
@@ -58,7 +139,7 @@ window.onload = function() {
 		  Start.innerHTML = "Play";
 	   }
    };
-  function addFifteenMins() 
+  function addFifteenMins()
   {
     if (running === false) {
         if (mins <= 45) {
@@ -67,7 +148,7 @@ window.onload = function() {
         }
     }
   }
-  function addFiveMins() 
+  function addFiveMins()
   {
     if (running === false) {
         if (mins <= 55) {
@@ -81,16 +162,7 @@ window.onload = function() {
         }
     }
  }
-  
-  
-  
 
- 
-  
-  
-  
-  
-  
    /*function addThirtySec() {
     if (running === 0) {
         if (sec < 60 && mins < 60) {
@@ -114,12 +186,8 @@ window.onload = function() {
 
     }
 }; */
-  
-  
-  
-  
-  
-  
+
+
 /*thirtyMins.onclick = function ()
  {
     Start.innerHTML = "Play";
@@ -129,48 +197,15 @@ window.onload = function() {
     secs = 0;
     tenths = 0;
     Timer.innerHTML = mins+":"+secs+":"+tenths;
-  }; 
-  
-  
-  
-  
-  
+  };
+
+
+
+
+
   */
-  
-  
-  function countBegins() {	 
-			time++;
-			tenths = time % 10;
-		   if(timeLeft == 0)
-		   {
-			   Timer.innerHTML = "00:00:00";
-		   }
-		   else
-		   {
-		 Timer.innerHTML = "00:"+parseInt(timeLeft,10)+":0"+tenths;
-		 timeLeft--;
-		   }
-  }
- 
-  
 
-  function increment() {
-    if(running === 1) {
-      setTimeout(function() {
-        time++;
-        mins = Math.floor(time / 10 / 60);
 
-        secs = Math.floor(time / 10 % 60);
-		if (mins < 10) {
-		  mins = "0" + mins;
-		}
-		if (secs < 10) {
-		  secs = "0" + secs;
-		}
-        tenths = time % 10;
-        Timer.innerHTML = mins+":"+secs+":"+"0"+tenths;
-        increment();
-      },100);
-    }
-  }
+
+
 }
